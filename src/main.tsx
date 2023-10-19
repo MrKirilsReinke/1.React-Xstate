a2994f0f5f2858f2ec484d61123669ccd1d07178
MrKirilsReinke <reinke.kirils@gmail.com>
2023-10-19 16:01:16 +0300

    Refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div className="grid grid-flow-col">
-    <BaseTrafficLight />
-    <BlinkingTrafficLight />
+      <BaseTrafficLight />
+      <BlinkingTrafficLight />
     </div>
   </React.StrictMode>
 );
