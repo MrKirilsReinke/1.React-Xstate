0954181447fe7c41449b90749aa43563e7c5574e
MrKirilsReinke <reinke.kirils@gmail.com>
2023-10-24 18:26:10 +0300

    Refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
-import BaseTrafficLight from "./assets/components/BaseTrafficLight.tsx";
+// import BaseTrafficLight from "./assets/components/BaseTrafficLight.tsx";
 // import HierarchicalBaseTrafficLight from "./assets/components/HierarchicalBaseTrafficLight";
 // import DelayedTrafficLight from "./assets/components/DelayedTrafficLight.tsx";
 // import ParallelStateTrafficLight from "./assets/components/ParallelStateTrafficLight.tsx";
-// import ContextActionsConditionsTrafficLight from "./assets/components/ContextActionsConditionsTrafficLight";
+import ContextActionsConditionsTrafficLight from "./assets/components/ContextActionsConditionsTrafficLight";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <div>
-    <BaseTrafficLight />
+    {/* <BaseTrafficLight /> */}
     {/* <HierarchicalBaseTrafficLight /> */}
     {/* <DelayedTrafficLight /> */}
     {/* <ParallelStateTrafficLight /> */}
-    {/* <ContextActionsConditionsTrafficLight /> */}
+    <ContextActionsConditionsTrafficLight />
   </div>
 );
